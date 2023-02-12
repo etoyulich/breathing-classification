43b105e7c220e3b9090d82d8b085a46714454856
Yulia Gomazkova <gomazkova19@yandex.ru>
2023-02-12 22:10:21 +0300

    Создание проекта

---
+++ src/main/java/com/breathingdetermination/BreathingDeterminationApplication.java
@@ -0,0 +1,13 @@
+package com.breathingdetermination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreathingDeterminationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreathingDeterminationApplication.class, args);
+    }
+
+}
