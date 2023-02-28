88bbdf78d08573aba76f62991121e53dbdd2417e
Yulia Gomazkova <gomazkova19@yandex.ru>
2023-02-28 18:57:01 +0300

    Добавление аннотации в тестовый класс

--- src/test/java/com/breathingdetermination/BreathingDeterminationApplicationTests.java
+++ src/test/java/com/breathingdetermination/BreathingDeterminationApplicationTests.java
@@ -1,9 +1,12 @@
 package com.breathingdetermination;
 
+import com.breathingdetermination.socket.DeterminationSocket;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = DeterminationSocket.class)
 class BreathingDeterminationApplicationTests {
 
     @Test
