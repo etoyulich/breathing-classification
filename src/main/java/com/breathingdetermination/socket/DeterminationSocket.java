5523aeeb8eac79ea629e882982b1f8fc9e66e103
Yulia Gomazkova <gomazkova19@yandex.ru>
2023-03-12 08:21:36 +0300

    Реализован метод onOpen

--- src/main/java/com/breathingdetermination/socket/DeterminationSocket.java
+++ src/main/java/com/breathingdetermination/socket/DeterminationSocket.java
@@ -1,5 +1,7 @@
 package com.breathingdetermination.socket;
 
+import com.breathingdetermination.util.Connection;
+import com.breathingdetermination.util.Coordinate;
 import com.breathingdetermination.util.DataRecord;
 import jakarta.websocket.*;
 import jakarta.websocket.server.PathParam;
@@ -21,7 +23,25 @@ public class DeterminationSocket {
 
     @OnOpen
     public void onOpen(Session session, @PathParam(value = "token") String token, @PathParam(value = "modelType") String modelType) throws IOException {
-
+        if (!Objects.equals(secretToken, token)) {
+            System.out.println("Введен неверный секретный токен.");
+            session.close(new CloseReason(CloseReason.CloseCodes.CANNOT_ACCEPT, "Введен неверный секретный токен."));
+            return;
+        }
+        if (!Objects.equals(modelType, "1") && !Objects.equals(modelType, "0")) {
+            System.out.println("Введен неправильный параметр типа используемой модели.");
+            session.close(new CloseReason(CloseReason.CloseCodes.CANNOT_ACCEPT, "Введен неправильный параметр типа используемой модели."));
+            return;
+        }
+        if (Connection.getInstance().getSession() != null) {
+            System.out.println("Невозможно подключиться к серверу. Уже существует открытое подключение.");
+            session.close(new CloseReason(CloseReason.CloseCodes.TRY_AGAIN_LATER, "Невозможно подключиться к серверу. Уже существует открытое подключение."));
+            return;
+        }
+        Connection.getInstance().setSession(session);
+        Connection.getInstance().setListener(new Socket());
+        this.modelType = modelType;
+        System.out.println("New session connected!");
     }
 
     @OnMessage
